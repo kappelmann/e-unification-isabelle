1157f87613dd58674f29f3d9e750b65928007d49
pabachmann <88983965+pabachmann@users.noreply.github.com>
2021-08-16 00:21:30 +0200

    renaming

--- Examples.thy
+++ Examples.thy
@@ -5,81 +5,83 @@ imports
 begin
 
 
-ML\<open>
+ML‹
   open Utils
-  val hint_unif = HO_Pat_Hint_Unif.h_unify;
+  val hint_unif = HO_Pat_Hint_Unif.hint_unify;
   val gen_ctxt = Context.the_generic_context
-\<close>
+›
 
-setup\<open>term_pat_setup\<close>
-declare [[log_level=600]]
+setup‹term_pat_setup›
+
+(* set to 600 to trace the hint unification procedure in detail *)
+declare [[log_level=500]]
 
 (* Simple Recursive Hint Unification Examples *)
 (* 1 *)
 lemma suc_one [hints]:
-  "x \<equiv> 1 \<Longrightarrow> Suc x \<equiv> 2"
+  "x ≡ 1 ⟹ Suc x ≡ 2"
 by linarith
 
 lemma add_zero [hints]:
-  "y \<equiv> z \<Longrightarrow> x \<equiv> 0 \<Longrightarrow> (y::nat) \<equiv> z + x"
+  "y ≡ z ⟹ x ≡ 0 ⟹ (y::nat) ≡ z + x"
 by simp
 
-ML\<open>
-  val (t1,t2) = (@{term_pat "Suc (?n + 0)"},@{term_pat "2::nat"})\<close>
+ML‹
+  val (t1,t2) = (@{term_pat "Suc (?n + 0)"},@{term_pat "2::nat"})›
   
-ML\<open>
+ML‹
   val (env,thm) = hint_unif (gen_ctxt ()) (t1,t2) (Envir.empty 0);
-  trace_unif_result @{context} (t1,t2) (env,thm)\<close>
+  trace_unif_result @{context} (t1,t2) (env,thm)›
 
 (* 2 *)
-consts f :: "nat \<Rightarrow> nat" g :: "nat \<Rightarrow> nat" h :: "nat \<Rightarrow> nat"
+consts f :: "nat ⇒ nat" g :: "nat ⇒ nat" h :: "nat ⇒ nat"
        a :: nat b :: nat
        
-lemma [hints]:"X \<equiv> f \<Longrightarrow> X a \<equiv> X b"
+lemma [hints]:"X ≡ f ⟹ X a ≡ X b"
   sorry
 
-lemma [hints]:"X \<equiv> Y \<Longrightarrow> h (g X) \<equiv> g (h Y)"
+lemma [hints]:"X ≡ Y ⟹ h (g X) ≡ g (h Y)"
   sorry
 
-ML\<open>
-  val (t1,t2) = (@{term_pat "h (g (f a))"},@{term_pat "g (h (f b))"})\<close>
+ML‹
+  val (t1,t2) = (@{term_pat "h (g (f a))"},@{term_pat "g (h (f b))"})›
   
-ML\<open>
+ML‹
   val (env,thm) = hint_unif (gen_ctxt ()) (t1,t2) (Envir.empty 0);
-  trace_unif_result @{context} (t1,t2) (env,thm)\<close>
+  trace_unif_result @{context} (t1,t2) (env,thm)›
 
 (* Simple Reflexive Tactics *)
 
 datatype Expr = Var int | Op Expr Expr
 
-fun eval :: "Expr \<Rightarrow> int" where
+fun eval :: "Expr ⇒ int" where
   "eval (Var i) = i"
 | "eval (Op ex1 ex2) = (eval ex1) + (eval ex2)"
 
-consts simpl :: "Expr \<Rightarrow> Expr"
+consts simpl :: "Expr ⇒ Expr"
 
 lemma soundness :
-  "P (eval (simpl x)) \<Longrightarrow> P (eval x)"
+  "P (eval (simpl x)) ⟹ P (eval x)"
 sorry
 
 
 (*supply base case and inductive hint*)
 lemma h_base [hints]:
-  "x \<equiv> Var i \<Longrightarrow> eval x \<equiv> i"
+  "x ≡ Var i ⟹ eval x ≡ i"
 by simp
 
 lemma h_add [hints]:
-  "x \<equiv> Op z1 z2 \<Longrightarrow> m \<equiv> eval z1 \<Longrightarrow> n \<equiv> eval z2 \<Longrightarrow> eval x \<equiv> m + n"
+  "x ≡ Op z1 z2 ⟹ m ≡ eval z1 ⟹ n ≡ eval z2 ⟹ eval x ≡ m + n"
 by simp
 
 
-ML\<open>
+ML‹
   val t1 = @{term_pat "eval ?y"};
-  val t2 = @{term_pat "a + (b + c) ::int"}\<close>
+  val t2 = @{term_pat "a + (b + c) ::int"}›
 
-ML\<open>
+ML‹
   val (env,thm) = hint_unif (gen_ctxt ()) (t1,t2) (Envir.empty 0);
-  trace_unif_result @{context} (t1,t2) (env,thm)\<close>
+  trace_unif_result @{context} (t1,t2) (env,thm)›
 
 
 (* Advanced Reflexive Tactics *)
@@ -89,41 +91,41 @@ datatype AdvExpr =
  |EMult AdvExpr AdvExpr
  |EOpp AdvExpr
 
-fun eval_adv :: "AdvExpr \<times> real list \<Rightarrow> real" where
-  "eval_adv (EUnit,\<Gamma>) = 1"
- |"eval_adv (EVar i,\<Gamma>) = \<Gamma>!i"
- |"eval_adv (EMult ex1 ex2,\<Gamma>) = eval_adv (ex1,\<Gamma>) * eval_adv (ex2,\<Gamma>)"
- |"eval_adv (EOpp ex,\<Gamma>) = inverse (eval_adv (ex,\<Gamma>))"
+fun eval_adv :: "AdvExpr × real list ⇒ real" where
+  "eval_adv (EUnit,Γ) = 1"
+ |"eval_adv (EVar i,Γ) = Γ!i"
+ |"eval_adv (EMult ex1 ex2,Γ) = eval_adv (ex1,Γ) * eval_adv (ex2,Γ)"
+ |"eval_adv (EOpp ex,Γ) = inverse (eval_adv (ex,Γ))"
 
 
 (*hints for heap lookup*)
 lemma h_var_base [hints]:
-  "x \<equiv> EVar 0 \<Longrightarrow> \<Gamma> \<equiv> n#\<Theta> \<Longrightarrow> eval_adv (x,\<Gamma>) \<equiv> n"
+  "x ≡ EVar 0 ⟹ Γ ≡ n#Θ ⟹ eval_adv (x,Γ) ≡ n"
 by simp
 
 lemma h_var_rec [hints]:
-  "x \<equiv> EVar (Suc p) \<Longrightarrow> \<Gamma> \<equiv> s#\<Delta> \<Longrightarrow> n \<equiv> eval_adv (EVar p,\<Delta>) \<Longrightarrow> eval_adv (x,\<Gamma>) \<equiv> n"
+  "x ≡ EVar (Suc p) ⟹ Γ ≡ s#Δ ⟹ n ≡ eval_adv (EVar p,Δ) ⟹ eval_adv (x,Γ) ≡ n"
 by simp
 
 (*hints for expressions*)
 lemma h_unit [hints]:
-  "x \<equiv> EUnit \<Longrightarrow> eval_adv (x,\<Gamma>) \<equiv> 1"
+  "x ≡ EUnit ⟹ eval_adv (x,Γ) ≡ 1"
 by simp
   
 lemma h_times [hints]:
-  "x \<equiv> EMult z1 z2 \<Longrightarrow> m \<equiv> eval_adv (z1,\<Gamma>) \<Longrightarrow> n \<equiv> eval_adv (z2,\<Gamma>) \<Longrightarrow> eval_adv (x,\<Gamma>) \<equiv> m * n"
+  "x ≡ EMult z1 z2 ⟹ m ≡ eval_adv (z1,Γ) ⟹ n ≡ eval_adv (z2,Γ) ⟹ eval_adv (x,Γ) ≡ m * n"
 by simp
 
 lemma h_opp [hints]:
-  "x \<equiv> EOpp z \<Longrightarrow> n \<equiv> eval_adv (z,\<Gamma>) \<Longrightarrow> eval_adv (x,\<Gamma>) \<equiv> inverse n"
+  "x ≡ EOpp z ⟹ n ≡ eval_adv (z,Γ) ⟹ eval_adv (x,Γ) ≡ inverse n"
 by simp
 
-ML\<open>
+ML‹
   val t1 = @{term_pat "eval_adv (?y,[3,5])"};
-  val t2 = @{term_pat "1 * inverse 3 * 5::real"}\<close>
+  val t2 = @{term_pat "1 * inverse 3 * 5::real"}›
 
-ML\<open>
+ML‹
   val (env,thm) = hint_unif (gen_ctxt ()) (t1,t2) (Envir.empty 0);
-  trace_unif_result @{context} (t1,t2) (env,thm)\<close>
+  trace_unif_result @{context} (t1,t2) (env,thm)›
 
 end
